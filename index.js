c9fd53132b91a5d51631c2ef345598eca572689b
phi <phi1618jp@gmail.com>
2020-04-06 17:39:55 +0900

    refactor

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ var main = async () => {
       current: (p) => {
         return path.join(__dirname, p);
       },
-      // 実行を開始したワーキングディレクトリ
-      working: (p) => {
-        return path.join(process.cwd(), p);
-      },
     }
   };
   
