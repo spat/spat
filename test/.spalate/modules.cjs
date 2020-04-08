990f6451933192ebc6ca7dc072b0cc5de7b5dedb
phi <phi1618jp@gmail.com>
2020-04-09 02:03:56 +0900

    build

--- test/.spalate/modules.cjs
+++ test/.spalate/modules.cjs
@@ -117,12 +117,144 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"tags/app.pug":[function(require,module,exports) {
+})({"../spalate.config.js":[function(require,module,exports) {
+module.exports = {
+  // env: 'staging',
+  server: {},
+  client: {
+    title: 'spalate with parcel'
+  }
+};
+},{}],"node_modules/@spalate/config.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _spalateConfig = _interopRequireDefault(require("../../../spalate.config.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// const config = require(`${process.cwd()}/spalate.config.js`);
+// import config from './spalate.config.js'
+_spalateConfig.default.env = _spalateConfig.default.env || 'temp';
+var _default = _spalateConfig.default;
+exports.default = _default;
+},{"../../../spalate.config.js":"../spalate.config.js"}],"node_modules/@spalate/spalate.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _browserOrNode = require("browser-or-node");
+
+console.log('isNode', _browserOrNode.isNode);
+console.log('isBrowser', _browserOrNode.isBrowser);
+var _default = {
+  isNode: _browserOrNode.isNode,
+  isBrowser: _browserOrNode.isBrowser
+};
+exports.default = _default;
+},{}],"node_modules/@spalate/server.mjs":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _spalate = _interopRequireDefault(require("./spalate.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// config
+const path = require('path');
+
+const express = require('express');
+
+const Bundler = require('parcel-bundler');
+
+const config = require('./config.js'); // spalate は global とする
+
+
+global.spalate = _spalate.default;
+
+var createParcelBundler = target => {
+  var config;
+
+  if (target === 'node') {
+    var entry = path.join(process.cwd(), 'app/server.js');
+    config = {
+      target: 'node',
+      bundleNodeModules: false,
+      outDir: `${SPALATE_OUTPUT_DIR}`,
+      outFile: 'modules.cjs',
+      hmr: false,
+      global: 'spalate',
+      cache: false,
+      sourceMaps: false
+    };
+  } else {
+    var entry = path.join(process.cwd(), 'app/index.js');
+    config = {
+      target: 'browser',
+      bundleNodeModules: true,
+      outDir: `${SPALATE_OUTPUT_DIR}/public`,
+      outFile: 'modules.js',
+      hmr: process.env.NODE_ENV !== 'production',
+      // 名前がバッティングするので off に
+      // global: 'spalate',
+      sourceMaps: true,
+      cache: false
+    };
+  }
+
+  var bundler = new Bundler(entry, config);
+  return bundler;
+};
+
+var SPALATE_OUTPUT_DIR = `${process.cwd()}/.spalate`; // setup node express
+
+const app = express(); // setup static
+
+app.use('/spalate', express.static(`${SPALATE_OUTPUT_DIR}/public`));
+app.use(express.static(`${process.cwd()}/public`)); // setup pug
+
+app.set('views', path.join(process.cwd(), 'views'));
+app.set('view engine', 'pug'); // setup parcel
+
+var bundler = createParcelBundler('browser');
+app.use(bundler.middleware()); // setup routing
+
+app.get('/', async (req, res) => {
+  // var ss = await db.collection('groups').get();
+  res.render('index', {});
+});
+var _default = app;
+exports.default = _default;
+},{"./config.js":"node_modules/@spalate/config.js","./spalate.js":"node_modules/@spalate/spalate.js"}],"tags/app.pug":[function(require,module,exports) {
 const riot = require('riot');
 
-riot.tag2('app', '<div class="p16"> <h1>{title}</h1> <div class="p16"> <div class="s64 bg-red"></div> </div> <div class="mb16"> <ul class="ml32"> <li>isNode: {isNode}</li> <li>isBrowser: {isBrowser}</li> <li each="{item in [1, 2, 3, 4]}">item {item}</li> </ul> </div> <div class="mb16"><img src="/images/kenkyo.png"></div> </div>', '', '', function (opts) {
+riot.tag2('app', '<div class="p16"> <h1>{title}</h1> <div class="p16"> <div class="s64 bg-red"></div> </div> <div class="mb16"> <ul class="ml32"> <li>isNode: {isNode}</li> <li>isBrowser: {isBrowser}</li> <li each="{item in [1, 2, 3, 4]}">item {item}</li> </ul> </div> <div class="mb16"> <ul class="ml32"> <li each="{item in items}">{item.data.title}</li> </ul> </div> <div class="mb16"><img src="/images/kenkyo.png"></div> </div>', '', '', function (opts) {
   this.title = 'Hello, spalate with parcel!';
-  this.on('mount', () => {});
+
+  var hoge = async () => {};
+
+  this.on('mount', async () => {
+    var ref = db.collection('groups');
+    var ss = await ref.get();
+    this.items = ss.docs.map(doc => {
+      return {
+        id: doc.id,
+        data: doc.data()
+      };
+    });
+    this.update();
+  });
 });
 },{}],"tags/*.pug":[function(require,module,exports) {
 module.exports = {
@@ -132,47 +264,56 @@ module.exports = {
 module.exports = {
   "app": require("./../app.pug")
 };
-},{"./../app.pug":"tags/app.pug"}],"scripts/main.js":[function(require,module,exports) {
+},{"./../app.pug":"tags/app.pug"}],"tags/index.js":[function(require,module,exports) {
 "use strict";
 
-var _browserOrNode = require("browser-or-node");
+require("./*.pug");
 
-var _riot = _interopRequireDefault(require("riot"));
+require("./**/*.pug");
+},{"./*.pug":"tags/*.pug","./**/*.pug":"tags/**/*.pug"}],"plugins/firebase.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _firebase = _interopRequireDefault(require("firebase"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-if (_browserOrNode.isBrowser) {
-  console.log('ブラウザだよ');
-  window.addEventListener('load', () => {
-    _riot.default.mount('*');
-  }, false);
-}
-},{}],"index.js":[function(require,module,exports) {
+const config = {
+  apiKey: "AIzaSyCiJjzLD4C5ueJ2wP3UVmdTa7BfiqnWu5Q",
+  authDomain: "chat-rabee-jp.firebaseapp.com",
+  databaseURL: "https://chat-rabee-jp.firebaseio.com",
+  projectId: "chat-rabee-jp",
+  storageBucket: "chat-rabee-jp.appspot.com",
+  messagingSenderId: "50741122756"
+};
+
+_firebase.default.initializeApp(config);
+
+var _default = _firebase.default;
+exports.default = _default;
+},{}],"server.js":[function(require,module,exports) {
 "use strict";
 
-require("./tags/*.pug");
+var _server = _interopRequireDefault(require("./node_modules/@spalate/server"));
 
-require("./tags/**/*.pug");
+require("./tags");
 
-var _firerest = _interopRequireDefault(require("firerest"));
-
-var _underscore = _interopRequireDefault(require("underscore"));
-
-var _riot = _interopRequireDefault(require("riot"));
-
-var _browserOrNode = require("browser-or-node");
-
-require("./scripts/main.js");
+var _firebase = _interopRequireDefault(require("~/plugins/firebase.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// グローバル化
-global._ = _underscore.default;
-global.riot = _riot.default;
-global.isBrowser = _browserOrNode.isBrowser;
-global.isNode = _browserOrNode.isNode;
-console.log('isNode', _browserOrNode.isNode);
-console.log('isBrowser', _browserOrNode.isBrowser); // export default {
-//   _,
-// };
-},{"./tags/*.pug":"tags/*.pug","./tags/**/*.pug":"tags/**/*.pug","./scripts/main.js":"scripts/main.js"}]},{},["index.js"], "spalate")
+var db = _firebase.default.firestore();
+
+global.db = db; // Start the server
+
+const PORT = process.env.PORT || 3000;
+
+_server.default.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+},{"./node_modules/@spalate/server":"node_modules/@spalate/server.mjs","./tags":"tags/index.js","~/plugins/firebase.js":"plugins/firebase.js"}]},{},["server.js"], "spalate")
