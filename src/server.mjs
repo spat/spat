7e7352f257e0e63c9b7558c15a8a2b3ea5fb3f40
phi <phi1618jp@gmail.com>
2020-04-13 04:36:36 +0900

    エラー時にデスクトップ通知が表示されるよう修正

--- src/server.mjs
+++ src/server.mjs
@@ -70,6 +70,34 @@ app.set('view engine', 'pug');
 var bundler = createParcelBundler('browser');
 app.use(bundler.middleware());
 
+// riot タグが更新されたら再読み込みする
+bundler.on('bundled', (bundle, a, b) => {
+  // console.log(Object.keys(bundle));
+  // console.log(bundle.parentBundle);
+
+  bundle.assets.forEach(item => {
+    // console.log(item.id);
+
+    if (/[^*]+\.pug$/.test(item.id)) {
+      // console.log(item.id);
+      // console.log(item.constructor);
+
+      eval(item.generated.js);
+    }
+  });
+});
+
+const notifier = require('node-notifier');
+bundler.on('buildError', error => {
+  // console.log(error);
+  notifier.notify({
+    'title': error.name,
+    'message': error.fileName,
+    
+    // 'title': error.title,
+    // 'message': error.message
+  });
+});
 
 // riot build
 import Ssriot from './ssriot.js'
