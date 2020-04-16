560d03e0f430d28520e91671b493bbfe712aa7fa
phi <phi1618jp@gmail.com>
2020-04-17 01:31:20 +0900

    refactor

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const fs = require('fs-extra');
-const path = require('path');
-const riot = require('riot');
-const express = require('express');
-const Bundler = require('parcel-bundler');
 const builder = require('./src/builder');
 
 var SPALATE_OUTPUT_DIR = `${process.cwd()}/.spalate`;
@@ -14,39 +9,12 @@ if (!process.env.PORT) {
 
 var main = async () => {
   if (!process.env.PORT) {
-    await bundleServerModules();
+    var bundler = builder.bundle('node');
+    await bundler.bundle();
   }
 
   // 全てまとめたファイルを実行
   require(`${SPALATE_OUTPUT_DIR}/modules.cjs`);
-};
-
-var createParcelBundler = () => {
-  var config;
-
-  var entry = path.join(process.cwd(), 'app/server.js');
-  config = {
-    target: 'node',
-    bundleNodeModules: false,
-    outDir: `${SPALATE_OUTPUT_DIR}`,
-    outFile: 'modules.cjs',
-    hmr: false,
-    global: 'spalate',
-    cache: false,
-    sourceMaps: false,  
-    watch: true,
-  };
-
-  var bundler = new Bundler(entry, config);
-
-  return bundler;
-};
-
-var bundleServerModules = async () => {
-  var bundler = createParcelBundler();
-
-
-  await bundler.bundle();
 };
 
 
--- src/builder.js
+++ src/builder.js
@@ -3,30 +3,51 @@
  */
 
 const fs = require('fs-extra');
+const path = require('path');
+const Bundler = require('parcel-bundler');
 
 // 実行パス直下の .spalate
 var SPALATE_OUTPUT_DIR = `${process.cwd()}/.spalate`;
 var SPALATE_APP_DIR = `${process.cwd()}/app`;
-
-var options = {
-  target: 'node',
-  outDir: `${SPALATE_OUTPUT_DIR}`,
-  outFile: 'modules.cjs',
-  bundleNodeModules: false,
-  hmr: false,
-  global: 'spalate',
-  cache: false,
-  sourceMaps: false,  
-  watch: true,
-};
 
 module.exports = {
   build() {
 
   },
 
-  bundle() {
+  bundle(target) {
+    if (target === 'node') {
+      var entry = path.join(process.cwd(), 'app/server.js');
+      config = {
+        target: 'node',
+        bundleNodeModules: false,
+        outDir: `${SPALATE_OUTPUT_DIR}`,
+        outFile: 'modules.cjs',
+        hmr: false,
+        global: 'spalate',
+        cache: false,
+        sourceMaps: false,
+        watch: true,
+      };
+    }
+    else {
+      var entry = path.join(process.cwd(), 'app/client.js');
+      config = {
+        target: 'browser',
+        bundleNodeModules: true,
+        outDir: `${SPALATE_OUTPUT_DIR}/static`,
+        outFile: 'modules.js',
+        hmr: process.env.NODE_ENV !== 'production',
+        // 名前がバッティングするので off に
+        // global: 'spalate',
+        sourceMaps: true,
+        cache: true,
+      };
+    }
+  
+    var bundler = new Bundler(entry, config);
 
+    return bundler;
   },
 
   copy() {
