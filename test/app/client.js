11613306b6618271a7df5d383568ae3296f41a13
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2020-09-14 17:20:38 +0900

    ページ遷移時のイベントが発火するようにしました

--- test/app/client.js
+++ test/app/client.js
@@ -2,3 +2,10 @@ import client from '.spat/modules/client';
 
 client.start();
 
+spat.appTag.on('pagechange', (e) => {
+  console.log(e);
+});
+
+spat.appTag.on('pagechanged', (e) => {
+  console.log(e);
+});
