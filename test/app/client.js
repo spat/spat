23ad515dfe25723610d929ea9f3e0cf12f9a9b98
phi <phi1618jp@gmail.com>
2020-11-14 19:51:13 +0900

    add console message

--- test/app/client.js
+++ test/app/client.js
@@ -3,9 +3,9 @@ import client from '.spat/modules/client';
 client.start();
 
 spat.appTag.on('pagechange', (e) => {
-  console.log(e);
+  console.log('pagechange', e);
 });
 
 spat.appTag.on('pagechanged', (e) => {
-  console.log(e);
+  console.log('pagechanged', e);
 });
