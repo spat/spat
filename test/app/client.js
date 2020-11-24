44e95733aee9dfca1b493d71eb72963e65dfc262
phi <phi1618jp@gmail.com>
2020-11-25 02:07:00 +0900

    indicator を追加

--- test/app/client.js
+++ test/app/client.js
@@ -1,11 +1,16 @@
 import client from '.spat/modules/client';
 
-client.start();
 
+var indicator = null;
 spat.appTag.on('pagechange', (e) => {
   console.log('pagechange', e);
+  indicator = spat.modal.indicator();
 });
 
 spat.appTag.on('pagechanged', (e) => {
   console.log('pagechanged', e);
+
+  indicator && indicator.close();
 });
+
+client.start();
