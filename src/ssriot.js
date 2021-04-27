61516b8ebaa3a63ca929f2176ca391dec2e379ce
phi <phi1618jp@gmail.com>
2021-04-28 04:42:17 +0900

    preload をキャッシュ

--- src/ssriot.js
+++ src/ssriot.js
@@ -58,6 +58,7 @@ module.exports = class Ssriot {
     var spat = {};
     spat.config = ${JSON.stringify(spat.config)};
     spat.plugins = ${JSON.stringify(spat.plugins)};
+    spat.preloadData = ${JSON.stringify(this.tag.navTag.preloadData)};
     </script>
     <script src="/spat/modules.js"></script>
 `;
