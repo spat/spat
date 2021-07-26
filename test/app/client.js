aa9eafc9e731e1339154ce45c7241150e6e68b83
simiraaaa <gmcthmy@gmail.com>
2021-07-26 18:01:18 +0900

    テストを修正

--- test/app/client.js
+++ test/app/client.js
@@ -4,13 +4,14 @@ import client from '.spat/modules/client';
 var indicator = null;
 spat.appTag.navTag.on('pagechange', (e) => {
   console.log('pagechange', e);
-  indicator = spat.modal.indicator();
+  indicator = indicator || spat.modal.indicator();
 });
 
 spat.appTag.navTag.on('pagechanged', (e) => {
   console.log('pagechanged', e);
 
   indicator && indicator.close();
+  indicator = null;
 });
 
 // client.showSSR();
